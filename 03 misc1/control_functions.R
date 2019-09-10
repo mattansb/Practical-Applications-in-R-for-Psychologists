905909baf8bcf493a0dd5d167819e0953d3b6782
mattansb <35330040+mattansb@users.noreply.github.com>
2019-09-10 12:24:24 +0300

    L3

---
+++ 03 misc1/control_functions.R
@@ -0,0 +1,165 @@
+
+# If else if --------------------------------------------------------------
+
+# ifelse:
+#
+# if (condition) {
+#   do something
+# } else if (another condition) {
+#   do something else
+# } else {
+#   find something else to do
+# }
+
+# e.g.,
+
+salary <- runif(1,min=1000,max=20000)
+
+if (salary < 5000) {
+  worker <- "poor"
+} else if (salary>15000) {
+  worker <- "rich"
+} else {
+  worker <- "so so"
+}
+
+salary
+worker
+
+# note: if..else is a control flow function. Takes only one logical value.
+# to use with a data frame: use ifelse (taking a vector as input).
+# compare this:
+
+if (data$Nerdness < 2) {
+  data$personality <- "cool"
+} else if (data$Nerdness>4) {
+  data$personality <- "nerd"
+} else {
+  data$personality <- "boring"
+}
+
+# to this:
+data$personality <- ifelse(data$Nerdness<2,"cool",ifelse(data$Nerdness>4,"nerd","boring"))
+
+
+# Loops -------------------------------------------------------------------
+
+
+
+# what does this code do?
+test <- numeric(length = 100)
+for (i in seq_along(test)) {
+  test[i] <- i^2
+}
+
+
+# what does this code do?
+num <- rnorm(100)
+min_num <- 1000
+
+for (i in seq_along(num)) {
+  if (num[i]<min_num) {
+    min_num <- num[i]
+  }
+}
+
+## pre-allocate.
+
+b <- NA
+system.time(
+  for (i in 1:1e6) {
+    b[i] <- rnorm(1)
+  }
+)
+
+b <- numeric(1e6)
+system.time(
+  for (i in 1:1e6) {
+    b[i] <- rnorm(1)
+  }
+)
+
+## vectorizing
+
+a <- numeric(1e6)
+system.time(
+  for (i in 1:1e6) {
+    a[i] <- rnorm(1)
+  }
+)
+
+system.time(a <- rnorm(1e6))
+
+# For most cases, you probably can do without loops...
+# see apply-family / purrr::map
+
+
+# functions ---------------------------------------------------------------
+
+# sum of two dice (from: "Hands-on programming with R")
+two_dice <- function() {
+  die <- 1:6
+  dice <- sample(die,2,replace = T)
+  sum(dice)  # the function will return the last command by default
+}
+
+two_dice
+two_dice()
+
+
+# sum of n dice
+n_dice <- function(n) {
+  die <- 1:6
+  dice <- sample(die,n,replace = T)
+  sum(dice)
+}
+
+n_dice(5)
+
+# default values:
+n_dice <- function(n = 2) {
+  die <- 1:6
+  dice <- sample(die,n,replace = T)
+  sum(dice)
+}
+
+n_dice()
+n_dice(5)
+
+# a new measure of distribution symmetry: mean/median.
+symmetry <- function(x) {
+  return(mean(x)/median(x))
+}
+
+a <- c(1,2,3,4,5,6,7,7,7,9,9,9,9,9)
+symmetry(a)
+
+# although this will also work (without "return"):
+symmetry <- function(x) {
+  mean(x)/median(x)
+}
+
+
+# few arguments:
+symmetry <- function(x, top="mean") {
+  if (top == "mean") {
+    return(mean(x)/median(x))
+  } else if (top == "median") {
+    return(median(x)/mean(x))
+  } else {
+    print("error")
+  }
+}
+
+symmetry(a)
+symmetry(a,top = "mean")
+symmetry(a,top="median")
+
+# or better:
+symmetry <- function(x, top="mean") {
+  switch(top,
+         mean = mean(x)/median(x),
+         median = median(x)/mean(x),
+         print("error")
+  )
+}
---
+++ 03 misc1/missing_data.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+
+tai_missing <- readRDS("tai_missing.Rds")
+head(tai_missing)
+
+#how many missing?
+tai_missing %>%
+  gather("Var","value",everything()) %>%
+  group_by(Var) %>%
+  summarise(missing_vals = mean(is.na(value)))
+
+# Test MCAR ---------------------------------------------------------------
+
+#' HOW TO TEST MISSING AT RANDOM??
+
+
+# Impute ------------------------------------------------------------------
+
+# https://www.analyticsvidhya.com/blog/2016/03/tutorial-powerful-packages-imputing-missing-values/
+
+## Hmisc ##
+
+library(Hmisc)
+
+# imputate single var
+tai_missing$calm_imp <- impute(tai_missing$calm, fun = mean)
+tai_missing$calm_imp <- impute(tai_missing$calm, fun = median)
+
+
+## MICE ##
+
+library(mice)
+
+md.pattern(tai_missing, plot = FALSE)
+
+imputed_Data <- mice(tai_missing, m = 5, maxit = 1, method = 'pmm', seed = 500)
+imputed_Data$imp %>% map(head)
+
+tai_imp_full <- complete(imputed_Data,action = 2)
+
+table(tai_imp_full$study,tai_missing$study, exclude = FALSE)
