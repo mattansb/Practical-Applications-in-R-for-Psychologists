e3e2798b50806c9786f6c90236bca3c3cbfca0fe
mattansb <35330040+mattansb@users.noreply.github.com>
2019-10-09 09:02:51 +0300

    more notes

--- 10 mediation & curvilinear/mediation.R
+++ 10 mediation & curvilinear/mediation.R
@@ -11,13 +11,21 @@ med_model <- mdt_simple(data = parental_iris,
 med_model
 add_index(med_model)
 
+# What would we make of an indirect effect w/o a total effect?
+# http://www.the100.ci/2019/10/03/indirect-effect-ex-machina/
+
 # see the models
 display_models(med_model)
 
 model_1 <- extract_model(med_model, step = 1)
 summary(model_1) # now do what ever you would do as usual!
 
-# for more complex models, you will need to use SEM...
+# Things become more complex with more complex models:
+# 1. For moderated mediation you can use `JSmediation::mdt_moderated()`.
+# 2. If you want to control for other variables in your mediations (other
+#    covariates), you can use `mediation::mediate()`
+# 3. For more complex mediation patterns, you will need to use SEM
+#    (`lavaan`, next semester...)
 
 # Exercise ----------------------------------------------------------------
 
