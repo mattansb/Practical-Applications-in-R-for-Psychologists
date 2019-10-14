df7123e27ead467351a0fa5b8005b110b975f927
mattansb <35330040+mattansb@users.noreply.github.com>
2019-10-14 11:00:32 +0300

    Update missing_data.R

--- 04 missing data/missing_data.R
+++ 04 missing data/missing_data.R
@@ -1,45 +1,43 @@
-library(dplyr)
 
 tai_missing <- readRDS("tai_missing.Rds")
 head(tai_missing)
 
-#how many missing?
-tai_missing %>%
-  summarise_all( ~ mean(is.na(.x)))
-
-
 # Test Patterns of Missingness --------------------------------------------
 
-#' How to test patterns of missingness (MCAR, MAR, MNAR)?
-#' It is out of the scope of our work here, but this is a really (really)
-#' good tutorial about detecting & visializing patterns of missingness
-#' using the `finalfit` package.
-#'
-#' https://finalfit.org/articles/missing.html
+library(finalfit)
 
+# How many missing?
+ff_glimpse(tai_missing)
+missing_plot(tai_missing)
 
-# Impute ------------------------------------------------------------------
+# How to test patterns of missingness (MCAR, MAR, MNAR)?
+# It is out of the scope of our work here, but this is a really (really)
+# good tutorial about detecting & visializing patterns of missingness
+# using the `finalfit` package:
+# https://finalfit.org/articles/missing.html
 
-# https://www.analyticsvidhya.com/blog/2016/03/tutorial-powerful-packages-imputing-missing-values/
-
-## Hmisc ##
+# Simple Imputation (Hmisc) -----------------------------------------------
 
 library(Hmisc)
 
-# imputate single var
+# imputate a single var with a single value
 tai_missing$calm_imp <- impute(tai_missing$calm, fun = mean)
+
+tai_missing$calm_imp # the `*` marks the imputated value...
+
 tai_missing$calm_imp <- impute(tai_missing$calm, fun = median)
+tai_missing$calm_imp <- impute(tai_missing$calm, fun = 1.2) # can also be a number
 
-
-## MICE ##
+# Multivariate Imputation (MICE) ------------------------------------------
 
 library(mice)
 
-md.pattern(tai_missing, plot = FALSE)
-
 imputed_Data <- mice(tai_missing, m = 5, maxit = 1, method = 'pmm', seed = 500)
-imputed_Data$imp %>% lapply(head)
+# (seed will guarantee you always get the same results)
 
-tai_imp_full <- complete(imputed_Data,action = 2)
+lapply(imputed_Data$imp, head) # see the first rows of
+
+tai_imp_full <- complete(imputed_Data, action = 2) # I chose the second imputation
 
 table(tai_imp_full$study,tai_missing$study, exclude = FALSE)
+
