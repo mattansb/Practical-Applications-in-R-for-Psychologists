669b212c7cb9b245716896b21b9dfff8f58d004e
mattansb <35330040+mattansb@users.noreply.github.com>
2019-12-14 08:29:47 +0200

    Lesson 5

--- 05 testing/htests.R
+++ 05 testing/htests.R
@@ -1,59 +1,72 @@
 library(psych) # for corr.test
-library(apa) # for cohens_d
+library(effectsize) # cohens_d and cramers_v
 library(ppcor) # for pcor and spcor
 library(BayesFactor) # all the Bayes...
 
 pdat <- readRDS("pdat.Rds")
 
+# This exercise will demo how to conduct the "classic" statistical tests,
+# along with their accompanying:
+#   - Confidence interval.
+#   - Standerdized effect size.
 
-# ttest -------------------------------------------------------------------
+
+# t-test ------------------------------------------------------------------
 
 ## between
-res <- t.test(pdat$Depression[pdat$Group == "a"],
-              pdat$Depression[pdat$Group == "b"], var.equal = TRUE)
-res
-cohens_d(res)
+t.test(pdat$Depression[pdat$Group == "a"],
+       pdat$Depression[pdat$Group == "b"], var.equal = TRUE)
+
+cohens_d(pdat$Depression[pdat$Group == "a"],
+         pdat$Depression[pdat$Group == "b"])
+
 
 ttestBF(pdat$Depression[pdat$Group == "a"],
         pdat$Depression[pdat$Group == "b"])
 
 ## within
 # order of values is crutial! TAKE CARE WHEN USING LONG DATA!
-res <- t.test(pdat$Cond_A, pdat$Cond_B, paired = TRUE)
-res
-cohens_d(res)
+t.test(pdat$Cond_A, pdat$Cond_B, paired = TRUE)
+
+cohens_d(pdat$Cond_A, pdat$Cond_B, paired = TRUE)
 
 ttestBF(pdat$Cond_A, pdat$Cond_B, paired = TRUE)
 
-# You can also set the width of the prior with `rscale`.
+# You can also set the width of the prior with the `rscale` argument.
 # Read more about selecting and reporting this, here:
 # http://xeniaschmalz.blogspot.com/2019/09/justifying-bayesian-prior-parameters-in.html
 
-# corr test ---------------------------------------------------------------
+
+# correlation -------------------------------------------------------------
 
 ## simple correlation
 
-cor.test(pdat$Depression, pdat$Joy)
+res <- cor.test(pdat$Depression, pdat$Joy)
+res
+
 correlationBF(pdat$Depression, pdat$Joy)
 
-# You can also set the width of the prior with `rscale`.
+# You can also set the width of the prior with the `rscale` argument.
 # Read more about selecting and reporting this, here:
 # http://xeniaschmalz.blogspot.com/2019/09/justifying-bayesian-prior-parameters-in.html
 
 cor.test(pdat$Depression, pdat$Joy, method = "spearman")
 
-# many at once
+# many at once (`corr.test` is NOT `cor.test`!)
 res <- corr.test(pdat[,c("Depression","Anxiety","Joy")])
 res
 res$ci
 
-corr.test(pdat[,c("Depression","Anxiety","Joy")], method = "spearman", use = "complete")
+corr.test(pdat[,c("Depression","Anxiety","Joy")],
+          method = "spearman", use = "complete")
 
 
-## partial
+# partial and part (semi-partial) correlations ----------------------------
+
+## partial correlation
 pcor(pdat[,c("Depression","Anxiety","Joy")])
 
-## part / semi_partial
+## part / semi-partial correlation
 spcor(pdat[,c("Depression","Anxiety","Joy")])
 
 # ci? :(
@@ -68,21 +81,16 @@ prop.test(sum(pdat$sex == "F"), nrow(pdat), p = 0.5)
 
 proportionBF(sum(pdat$sex == "F"), nrow(pdat), p = 0.5)
 
-# chi test ----------------------------------------------------------------
+
+# chi-squared test --------------------------------------------------------
 
 res <- chisq.test(pdat$sex, pdat$Group, correct = FALSE)
 res
 
-
-cramers_v <- function(chisq.test.res){
-  chisq <- unname(chisq.test.res$statistic)
-  n <- sum(chisq.test.res$observed)
-  k <- min(dim(chisq.test.res$observed))
-
-  c("cramers_v" = sqrt(chisq / (n * (k - 1))))
-}
-
-cramers_v(res)
+chisq_to_cramers_v(chisq = res$statistic,
+                   n = sum(res$observed),
+                   nrow = nrow(res$observed),
+                   ncol = ncol(res$observed))
 
 
 contingencyTableBF(table(pdat$sex, pdat$Group), sampleType = "jointMulti")
@@ -91,11 +99,8 @@ contingencyTableBF(table(pdat$sex, pdat$Group), sampleType = "jointMulti")
 # Exercise ----------------------------------------------------------------
 
 
-# 1. Both frequentist and bayesian tests support one sided test...
-#    conduct a one sided:
-#    - t test
-#    - correlation test
-#    - proportion test
-#    How do these affect the results?
-# 2. Load one of the files from prevoius lessons - conduct 2 tests of
-#    your choosing.
+# Both frequentist and bayesian tests support one sided tests...
+# Conduct a one sided:
+#  - t test
+#  - correlation test
+# How do these affect the results?
