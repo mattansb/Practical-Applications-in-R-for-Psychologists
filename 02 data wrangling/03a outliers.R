6c1760a44a3988586191dddf77b1c335799fd6e7
mattansb <35330040+mattansb@users.noreply.github.com>
2020-08-19 15:49:58 +0300

    Update 03a outliers.R

--- 02 data wrangling/03a outliers.R
+++ 02 data wrangling/03a outliers.R
@@ -1,17 +1,22 @@
 
 library(dplyr)
 
-
 # There are many rules of thumb for defining what an outlier IS (relative
 # thresholds, absolute thresholds). For the purposes of this demo, an outlier
-# will be defined as any observation that is 1.2 SDs from the mean (this is NOT
-# a good rule of thumb!).
+# will be defined as any observation that is 1.1 IQRs (interquartile range) the
+# median (this is NOT a good rule of thumb!).
 
 tai_missing <- readRDS("tai_missing.Rds")
 head(tai_missing)
 
-hist(tai_missing$moED)
-abline(v = c(-1.2, 1.2)*sd(tai_missing$moED) + mean(tai_missing$moED), col = "red")
+(moEd_range <- tai_missing %>%
+    summarise(range = c(-1.1,1.1) * IQR(moED) + median(moED)))
+
+
+{
+  hist(tai_missing$moED)
+  abline(v = moEd_range$range, col = "red")
+}
 
 # Here we will explore two popular options for dealing with outliers.
 
@@ -21,8 +26,8 @@ abline(v = c(-1.2, 1.2)*sd(tai_missing$moED) + mean(tai_missing$moED), col = "re
 # This is the easiest option - remove them!
 
 tai_no_OL <- tai_missing %>%
-  mutate(moED_z = scale(moED),
-         moEd_ol = abs(moED_z) > 1.2) %>%
+  mutate(moED_std_d = (moED - median(moED)) / IQR(moED),
+         moEd_ol = moED_std_d > 1.1 | moED_std_d < -1.1) %>%
   filter(!moEd_ol)
 
 
@@ -37,14 +42,28 @@ library(DescTools) # for `Winsorize()`
 
 tai_winzorize_OL <- tai_missing %>%
   mutate(moED_win = Winsorize(moED,
-                              minval = -1.2 * sd(moED) + mean(moED),
-                              maxval = 1.2 * sd(moED) + mean(moED)))
-
+                              minval = -1.1 * IQR(moED) + median(moED),
+                              maxval = 1.1 * IQR(moED) + median(moED)))
 
 # Compare -----------------------------------------------------------------
 
 
-hist(tai_missing$moED, xlim = c(0, 25))
-hist(tai_no_OL$moED, xlim = c(0, 25))
-hist(tai_winzorize_OL$moED_win, xlim = c(0, 25))
+{
+  par(mfrow = c(3, 1))
 
+  hist(tai_missing$moED, main = "Original",
+       xlim = c(0, 25), breaks = seq(0,25,1))
+  abline(v = moEd_range$range, col = "red")
+
+  hist(tai_no_OL$moED, main = "Omit",
+       xlim = c(0, 25), breaks = seq(0,25,1))
+  abline(v = moEd_range$range, col = "red")
+
+  hist(tai_winzorize_OL$moED_win, main = "Winsorize",
+       xlim = c(0, 25), breaks = seq(0,25,1))
+  abline(v = moEd_range$range, col = "red")
+
+  par(mfrow = c(1, 1))
+}
+
+# next lesson we will see better plotting methods...
