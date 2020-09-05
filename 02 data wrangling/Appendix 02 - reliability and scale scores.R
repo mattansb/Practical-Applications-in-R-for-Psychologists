797ac8a3e0857e1756ff29d0ec5fe5f8f92d097e
mattansb <35330040+mattansb@users.noreply.github.com>
2020-09-05 19:02:09 +0300

    Update Appendix 02 - reliability and scale scores.R

--- 02 data wrangling/Appendix 02 - reliability and scale scores.R
+++ 02 data wrangling/Appendix 02 - reliability and scale scores.R
@@ -16,10 +16,15 @@ df_NPAS %>%
 # Use `across` to compute sum / mean scores
 df_NPAS_with_score <- df_NPAS %>%
   mutate(
-    Nerdy1 = rowSums(across(.cols = Q1:Q26), na.rm = TRUE),
-    Nerdy2 = rowMeans(across(.cols = Q1:Q26), na.rm = TRUE) # better for missing data
+    # Raw Sum/Mean scores
+    Nerd_sum_raw = rowSums(across(Q1:Q26), na.rm = TRUE),
+    Nerd_mean_raw = rowMeans(across(Q1:Q26), na.rm = TRUE), # better for missing data
+    # Standardized Sum/Mean scores
+    Nerd_sum_std = rowSums(across(Q1:Q26, .fns = scale), na.rm = TRUE),
+    Nerd_mean_std = rowMeans(across(Q1:Q26, .fns = scale), na.rm = TRUE), # better for missing data
   ) %>%
   select(-(Q1:Q26))
 
 head(df_NPAS_with_score)
 
+# For more reliability measures see the `psych` and `irr` packages.
