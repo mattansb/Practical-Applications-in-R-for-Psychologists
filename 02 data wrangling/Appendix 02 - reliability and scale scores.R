a0c9e4fd4595b39049d8d35c4ca71f4f11775193
mattansb <35330040+mattansb@users.noreply.github.com>
2020-10-29 15:15:21 +0200

    clean NPAS

--- 02 data wrangling/Appendix 02 - reliability and scale scores.R
+++ 02 data wrangling/Appendix 02 - reliability and scale scores.R
@@ -3,28 +3,27 @@
 
 library(dplyr)
 
-
-df_NPAS <- readRDS("NPAS-data_clean.Rds")
-glimpse(df_NPAS)
+NPAS <- read.csv("data/NPAS.csv") # data from https://osf.io/5njdx/
+glimpse(NPAS)
 
 # Use `psych::alpha` to compute Cronbach's Alpha:
-df_NPAS %>%
-  select(Q1:Q26) %>%
+NPAS %>%
+  select(NPAS_1:NPAS_26) %>%
   psych::alpha()
 
 
 # Use `across` to compute sum / mean scores
-df_NPAS_with_score <- df_NPAS %>%
+NPAS_with_score <- NPAS %>%
   mutate(
     # Raw Sum/Mean scores
-    Nerd_sum_raw = across(Q1:Q26) %>% rowSums(na.rm = TRUE),
-    Nerd_mean_raw = across(Q1:Q26) %>% rowMeans(na.rm = TRUE), # better for missing data
+    Nerd_sum_raw = across(NPAS_1:NPAS_26) %>% rowSums(na.rm = TRUE),
+    Nerd_mean_raw = across(NPAS_1:NPAS_26) %>% rowMeans(na.rm = TRUE), # better for missing data
     # Standardized Sum/Mean scores
-    Nerd_sum_std = across(Q1:Q26, .fns = scale) %>% rowSums(na.rm = TRUE),
-    Nerd_mean_std = across(Q1:Q26, .fns = scale) %>% rowMeans(na.rm = TRUE), # better for missing data
+    Nerd_sum_std = across(NPAS_1:NPAS_26, .fns = scale) %>% rowSums(na.rm = TRUE),
+    Nerd_mean_std = across(NPAS_1:NPAS_26, .fns = scale) %>% rowMeans(na.rm = TRUE), # better for missing data
   ) %>%
-  select(-(Q1:Q26))
+  select(-(NPAS_1:NPAS_26))
 
-head(df_NPAS_with_score)
+head(NPAS_with_score)
 
 # For more reliability measures see the `psych` and `irr` packages.
